a83b8d0713b4e33def57aa09fe1cb338b5f7fecf
Tetiana Boichenko <boichenko.tetiana@gmail.com>
2020-10-05 17:43:03 +0300

    checking workflow for the hook to syscall close

--- test.c
+++ test.c
@@ -1,7 +1,9 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
 
-int (* current)(const char *,int, int);
+int (* syshook_open)(const char *,int, int);
+int (* syshook_close)(int);
 
 extern void *(* __malloc_hook)(size_t __size, const void *);
 
@@ -9,14 +11,21 @@ int patched(const char *file, int oflag, int mode) {
 
 	printf("In patched open()\n");
 
-	return current(file, oflag, mode);
+    return syshook_open(file, oflag, mode);
+}
+
+int patched_close(int fd) {
+
+    printf("In patched close()\n");
+
+    return syshook_close(fd);
 }
 
 int main() {
 
 	void * (* test)(size_t, const void *) = __malloc_hook;
 
-	current = syscall_open;
+    syshook_open = syscall_open;
 
 	syscall_open = patched;
 
@@ -25,6 +34,11 @@ int main() {
 	printf ("%i\n", f);
 
 	FILE * fp = fopen("/dev/null", "rb");
+
+    syshook_close = syscall_close;
+    syscall_close = patched_close;
+
+    close(f);
 
 	return 0;
 }
