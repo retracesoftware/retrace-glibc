1a12e9a403ee63ac0782a5647bdebfbd5b628188
Tetiana Boichenko <boichenko.tetiana@gmail.com>
2020-10-07 12:04:45 +0300

    checking workflow for the hook to syscall write () todo: find the reason for Segmentation fault (SIGSEGV)

--- test.c
+++ test.c
@@ -6,6 +6,7 @@
 int (* syshook_open)(const char *,int, int);
 int (* syshook_close)(int);
 ssize_t (* syshook_read)(int,void *,size_t);
+ssize_t (* syshook_write)(int,const void *,size_t);
 
 extern void *(* __malloc_hook)(size_t __size, const void *);
 
@@ -28,28 +29,41 @@ ssize_t patched_read(int fd, void *buf, size_t nbytes) {
 
     return syshook_read(fd,buf,nbytes);
 }
+ssize_t patched_write(int fd, const void *buf, size_t nbytes) {
 
+    //printf("In patched write()\n");
 
+    return syshook_write(fd,buf,nbytes);
+}
 
-int main() {
-
-	void * (* test)(size_t, const void *) = __malloc_hook;
-
+void init_hooks(){
     syshook_open = syscall_open;
-
-	syscall_open = patched;
-
-	int f = open("/dev/null", O_RDONLY);
-
-	printf ("%i\n", f);
-
-	FILE * fp = fopen("/dev/null", "rb");
+    syscall_open = patched;
 
     syshook_read = syscall_read;
     syscall_read = patched_read;
 
+    syshook_write = syscall_write;
+    syscall_write = patched_write;
+
     syshook_close = syscall_close;
     syscall_close = patched_close;
+}
+#define TEST_FILE "file_for_test"
+int main() {
+
+	void * (* test)(size_t, const void *) = __malloc_hook;
+
+    init_hooks();
+
+
+    int f = open(TEST_FILE, O_RDONLY);
+
+    printf ("%i\n", f);
+
+    FILE * fp = fopen(TEST_FILE, "rb");
+
+
 
     close(f);
 
